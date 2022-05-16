dc6d56742ddb0ea0a183bcde151b1ffaa8c3d614
Pablo Saunite de Souza <pablosaunite@gmail.com>
2022-05-16 20:28:58 -0300

    update authentication method with mail service

--- src/models/email.js
+++ src/models/email.js
@@ -1,18 +1,30 @@
 const process = require("process");
 const nodemailer = require("nodemailer");
+const {google} = require("googleapis");
 
-const transporterConfiguration = {
+const getEmailConfiguration = accessToken => ({
     service: process.env.EMAIL_SERVICE,
     auth: {
+        type: process.env.EMAIL_AUTH_TYPE,
         user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASSWORD
+        pass: process.env.EMAIL_PASSWORD,
+        clientId: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+        refreshToken: process.env.REFRESH_TOKEN,
+        accessToken: accessToken
     }
-}
+})
 
-const transporter = nodemailer.createTransport(transporterConfiguration);
+const oAuth2Client = new google.auth.OAuth2(
+    getEmailConfiguration().auth.clientId,
+    getEmailConfiguration().auth.clientSecret,
+);
+oAuth2Client.setCredentials({refresh_token: getEmailConfiguration().auth.refreshToken})
 
 const _sendEMail = async function({to, subject, text, html, cc, files}) {
     try {
+        const accessToken = await oAuth2Client.getAccessToken();
+        const transporter = nodemailer.createTransport(getEmailConfiguration(accessToken));
         return new Promise((res, rej) => {
             transporter.sendMail({
                 from: {
